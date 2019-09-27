421c0a348b1f2f029e8e139b472ccfa2d798ed7c
karimov <karimov@bankexim.com>
2019-09-27 09:39:05 +0300

    add js file

---
+++ www/deviceinfo.js
@@ -0,0 +1,21 @@
+
+// Empty constructor
+function DeviceInfo() {}
+
+// The function that passes work along to native shells
+// Message is a string, duration may be 'long' or 'short'
+DeviceInfo.prototype.show = function(operation, successCallback, errorCallback) {
+  var options = {};
+    options.operation = operation;
+    cordova.exec(successCallback, errorCallback, 'DeviceInfo', 'get', [options]);
+}
+
+// Installation constructor that binds DeviceInfo to window
+DeviceInfo.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+    window.plugins.deviceInfo = new DeviceInfo();
+    return window.plugins.deviceInfo;
+};
+cordova.addConstructor(DeviceInfo.install);
